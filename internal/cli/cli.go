a77f44f93951c535eb3e243e722fa324f595cde4
stewthestew <stew156@proton.me>
2025-05-06 11:14:53 +0000

    chore: removed debug print statement

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,7 +1,6 @@
 package cli
 
 import (
-	"fmt"
 	"os"
 
 	flag "github.com/spf13/pflag"
@@ -17,7 +16,7 @@ func ParseFlags() CliOptions {
 	flag.BoolVarP(&cliOptions.List, "list", "l", false, "List available languages")
 	flag.StringVarP(&cliOptions.Name, "name", "n", "", "Name of the language preset to initialize")
 	flag.Parse()
-	fmt.Println(cliOptions.List, cliOptions.Name)
+
 	if cliOptions.Name == "" && cliOptions.List == false {
 		flag.Usage()
 		os.Exit(1) // Just incase since I don't know is flag.Usage() exits or not
